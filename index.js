e290066450633b08c9e907e6b17d76fdd2ea0289
Grace Brackman <gbrackma@purdue.edu>
2018-05-14 14:51:28 -0400

    add the super bonus and change font

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 
 const button = document.querySelector('button')
 const subHeader = document.querySelector('h1.subHeading')
+//let submitText = document.forms["newText"].value;
+
+const myForm = document.getElementById("myForm");
+//document.myForm.newText
+const submitted = document.myForm.submitButton
+
+
+
+submitted.addEventListener('click', function(ev){
+    let change = document.myForm.myText.value
+    subHeader.textContent = change
+})
 
 button.addEventListener('click', function(ev){
     subHeader.textContent = "You have clicked my beautiful button! Thank you!"
   })
+
+  
