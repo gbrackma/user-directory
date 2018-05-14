f97f3bc389cd588fb1746d1d914155c17361612b
Grace Brackman <gbrackma@purdue.edu>
2018-05-14 13:00:07 -0400

    did homework and bonus

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("it works")
+//console.log("it works")
+
+const button = document.querySelector('button')
+const subHeader = document.querySelector('h1.subHeading')
+
+button.addEventListener('click', function(ev){
+    subHeader.textContent = "You have clicked my beautiful button! Thank you!"
+  })
