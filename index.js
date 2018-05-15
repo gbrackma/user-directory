735f8d6fc28fe9862969ebbeeecbe487e37539bd
Grace Brackman <gbrackma@purdue.edu>
2018-05-15 13:24:38 -0400

    seperate submit handler into renderList and renderListItem and renderColor

--- index.js
+++ index.js
@@ -20,40 +20,62 @@ button.addEventListener('click', function(ev){
   })
 
   //name code
+  // *************** HOMEWORK FOR 5/15 BELOW THIS LINE ***********************
   const form = document.querySelector("#userForm");
   form.addEventListener("submit", function(ev){
       ev.preventDefault()
-      const heading = document.querySelector("#users")
-      const user = ev.target.userName
-      const age = ev.target.age
-      const color = ev.target.favColor
 
-      const list = document.createElement('ul')
+      //get the variables
+      const users = document.querySelector("#users")
+      const user = ev.target.userName.value
+      const age = ev.target.age.value
+      const color = ev.target.favColor.value
 
-      const nameElement = document.createElement('li')
-      nameElement.textContent = `Name: ${user.value}`
-      list.appendChild(nameElement)
+      //call render list
+      const list = renderlist(users, user, age, color)
 
-      const ageElement = document.createElement('li')
-      ageElement.textContent = `Age: ${age.value}`
-      list.appendChild(ageElement)
+      //add list to the div
+      users.appendChild(list)
 
-      const colorItem = document.createElement('li')
-      colorItem.textContent = `Favorite Color: `
-      const colorDiv = document.createElement('div')
-      colorDiv.style.backgroundColor = color.value
-      colorDiv.style.width = '3rem'
-      colorDiv.style.height = '3rem'
-      colorItem.appendChild(colorDiv)
-      list.appendChild(colorItem)
-
-      //list.style.backgroundColor = color.value
-      heading.appendChild(list)
-
-      //heading.innerHTML += `<p>${user.value}, ${age.value}</p>`
+      //reset form
       form.reset()
       form.userName.focus()
   })
+
+  function renderlist(users, user, age, color){
+
+    //create the list
+    const list = document.createElement('ul')
+    //call renderListItem for name and age
+    list.appendChild(renderListItem('Name', user))
+    list.appendChild(renderListItem('Age', age))
+
+    //create color Item
+    const colorItem = renderListItem('Favorite Color','')
+    //add color box to color item
+    colorItem.appendChild(rendercolor(color))
+    //append color 
+    list.appendChild(colorItem)
+
+
+    return list
+
+
+  }
+
+  function renderListItem(label, item){
+    const element = document.createElement('li')
+    element.textContent = `${label}: ${item}`
+    return element
+  }
+
+  function rendercolor(newColor){
+    const colorDiv = document.createElement('div')
+    colorDiv.style.backgroundColor = newColor
+    colorDiv.style.width = '3rem'
+    colorDiv.style.height = '3rem'
+    return colorDiv
+  }
 
 
   
