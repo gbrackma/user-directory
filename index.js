53017b7ada2be0f55111155443aac187b70aec55
Grace Brackman <gbrackma@purdue.edu>
2018-05-15 13:42:41 -0400

    clean up renderItem and renderList and center the text

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 //console.log("it works")
 
-const button = document.querySelector('button')
-const subHeader = document.querySelector('h1.subHeading')
-//let submitText = document.forms["newText"].value;
 
-const myForm = document.getElementById("myForm");
-//document.myForm.newText
-const submitted = document.myForm.submitButton
-
-
-
-submitted.addEventListener('click', function(ev){
-    let change = document.myForm.myText.value
-    subHeader.textContent = change
-})
-
-button.addEventListener('click', function(ev){
-    subHeader.textContent = "You have clicked my beautiful button! Thank you!"
-  })
 
   //name code
   // *************** HOMEWORK FOR 5/15 BELOW THIS LINE ***********************
