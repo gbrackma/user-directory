c8967ed65924ac6e16a5f2023e88e834e47a840b
Grace Brackman <gbrackma@purdue.edu>
2018-05-16 09:44:14 -0400

    better comment all functions

--- index.js
+++ index.js
@@ -6,15 +6,16 @@
   // *************** HOMEWORK FOR 5/15 BELOW THIS LINE ***********************
   const form = document.querySelector("#userForm");
   form.addEventListener("submit", function(ev){
+      //called with submit is clicked
       ev.preventDefault()
 
-      //get the variables
+      //get the variables from the form
       const users = document.querySelector("#users")
       const user = ev.target.userName.value
       const age = ev.target.age.value
       const color = ev.target.favColor.value
 
-      //call render list
+      //call render list using variables
       const list = renderlist(users, user, age, color)
 
       //add list to the div
@@ -26,6 +27,7 @@
   })
 
   function renderlist(users, user, age, color){
+      //creates a users list item to append to the list of users (users)
 
     //create the list
     const list = document.createElement('ul')
@@ -47,12 +49,15 @@
   }
 
   function renderListItem(label, item){
+      //creates a list item and returns it 
     const element = document.createElement('li')
     element.textContent = `${label}: ${item}`
     return element
   }
 
   function rendercolor(newColor){
+      //creates a color box from favColor of the user
+      //returns div of new color
     const colorDiv = document.createElement('div')
     colorDiv.style.backgroundColor = newColor
     colorDiv.style.width = '10rem'
